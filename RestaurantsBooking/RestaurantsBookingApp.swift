1f75b1a55b808b4fcb6efca6a418950213282ca4
Masud Rana <masud.pust16@gmail.com>
2023-03-16 23:09:58 +0600

    Initial commit

---
+++ RestaurantsBooking/RestaurantsBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsBookingApp.swift
+//  RestaurantsBooking
+//
+//  Created by Md. Masud Rana on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
