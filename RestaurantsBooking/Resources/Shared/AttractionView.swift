7b268a7c48fea1be7e99069a841045e3af116087
Masud Rana <masud.pust16@gmail.com>
2023-03-17 00:26:10 +0600

    Change name and complete attractionView

--- RestaurantsBooking/Resources/Shared/AttractionView.swift
+++ RestaurantsBooking/Resources/Shared/AttractionView.swift
@@ -1,34 +1,37 @@
 //
-//  UserAttractionView.swift
+//  AttractionView.swift
 //  RestaurantsBooking
 //
-//  Created by Md. Masud Rana on 3/16/23.
+//  Created by Md. Masud Rana on 3/17/23.
 //
 
 import SwiftUI
 
-struct UserAttractionView: View {
+struct AttractionView: View {
+    private var iPhone = UIDevice.current.userInterfaceIdiom == .phone
+
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
             Image("Tracking_Map")
                 .resizable()
                 .scaledToFit()
                 .padding(.bottom, 50)
-            
+
             Text("Nearby restaurants")
                 .font(.system(size: 24, weight: .semibold))
                 .padding(.bottom, 16)
-            
+                .foregroundColor(Color.label)
+
             Text("You don't have to go far to find a good restaurant,we have provided all the restaurants that is near you")
                 .multilineTextAlignment(.center)
                 .font(.system(size: 12, weight: .regular))
-                .foregroundColor(Color.secondary)
+                .foregroundColor(Color.secondaryLabel)
         }
     }
 }
 
-struct UserAttractionView_Previews: PreviewProvider {
+struct AttractionView_Previews: PreviewProvider {
     static var previews: some View {
-        UserAttractionView()
+        AttractionView()
     }
 }
---
+++ RestaurantsBooking/Resources/Shared/CommonButton.swift
@@ -0,0 +1,25 @@
+//
+//  CommonButton.swift
+//  RestaurantsBooking
+//
+//  Created by Md. Masud Rana on 3/17/23.
+//
+
+import SwiftUI
+
+struct CommonButton: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            
+        }
+
+    }
+}
+
+struct CommonButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CommonButton()
+    }
+}
