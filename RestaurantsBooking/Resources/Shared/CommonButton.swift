a749ca0abee244c22572fa9d9cccb497bbecffce
Masud Rana <masud.pust16@gmail.com>
2023-03-17 21:45:12 +0600

    add common button

--- RestaurantsBooking/Resources/Shared/CommonButton.swift
+++ RestaurantsBooking/Resources/Shared/CommonButton.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct CommonButton: View {
-    var body: some View {
-        Button {
-            
-        } label: {
-            
-        }
+    private var title: String
+    private var backgroundColor: Color = .systemGreen
+    private var foregroundColor: Color = .systemWhite
+    var onClickAction: (() -> Void)?
 
+    init(
+        title: String,
+        backgroundColor: Color = Color.systemGreen,
+        foregroundColor: Color = Color.systemWhite,
+        onClickAction: (() -> Void)? = nil
+    ) {
+        self.title = title
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.onClickAction = onClickAction
+    }
+
+    var body: some View {
+        VStack {
+            Button {} label: {
+                Text(title)
+                    .font(.system(size: 14, weight: .bold))
+                    .padding(.horizontal, 74)
+                    .padding(.vertical, 16)
+                    .background(backgroundColor)
+                    .cornerRadius(12)
+                    .foregroundColor(foregroundColor)
+            }
+        }
     }
 }
 
 struct CommonButton_Previews: PreviewProvider {
     static var previews: some View {
-        CommonButton()
+        CommonButton(title: "Create Account")
     }
 }
