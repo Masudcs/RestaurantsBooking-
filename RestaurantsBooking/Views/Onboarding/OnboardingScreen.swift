3feb87095efae3fbba8aef0d9b0f82367d067025
Masud Rana <masud.pust16@gmail.com>
2023-04-09 12:03:56 +0600

    add navigationview on OnboardingScreen

--- RestaurantsBooking/Views/Onboarding/OnboardingScreen.swift
+++ RestaurantsBooking/Views/Onboarding/OnboardingScreen.swift
@@ -11,62 +11,66 @@ struct OnboardingScreen: View {
     @State var tabIndex = 0
     
     var body: some View {
-        VStack(spacing: 0) {
-            TabView(selection: $tabIndex) {
-                NearbyRestaurantsView()
-                    .tag(0)
-                FavoritesMenuView()
-                    .tag(1)
-                FoodView()
-                    .tag(2)
-                WelcomeView()
-                    .tag(3)
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            
-            if tabIndex != 3 {
-                Spacer()
+        NavigationView {
+            VStack(spacing: 0) {
                 
-                HStack(spacing: 0) {
-                    Button {
-                        tabIndex = 3
-                    } label: {
-                        Text("Skip")
-                            .font(.system(size: 16, weight: .regular))
-                            .foregroundColor(Color.secondaryLabel)
-                    }
+                TabView(selection: $tabIndex) {
+                    NearbyRestaurantsView()
+                        .tag(0)
+                    FavoritesMenuView()
+                        .tag(1)
+                    FoodView()
+                        .tag(2)
                     
-                    Spacer()
-                    
-                    HStack {
-                        Circle()
-                            .fill(tabIndex == 0 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
-                            .frame(width: 8, height: 8)
-                        
-                        Circle()
-                            .fill(tabIndex == 1 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
-                            .frame(width: 8, height: 8)
-                        
-                        Circle()
-                            .fill(tabIndex == 2 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
-                            .frame(width: 8, height: 8)
-                    }
-                    
-                    Spacer()
-                    
-                    Button {
-                        tabIndex += 1
-                    } label: {
-                        Image(systemName: "arrow.forward")
-                            .foregroundColor(Color.systemGreen)
-                            .font(.system(size: 16, weight: .regular))
-                    }
+                    WelcomeView()
+                        .tag(3)
                 }
-                .padding(.bottom, 44)
-                .padding(.horizontal, 16)
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                
+                if tabIndex != 3 {
+                    Spacer()
+                    
+                    HStack(spacing: 0) {
+                        Button {
+                            tabIndex = 3
+                        } label: {
+                            Text("Skip")
+                                .font(.system(size: 16, weight: .regular))
+                                .foregroundColor(Color.secondaryLabel)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Circle()
+                                .fill(tabIndex == 0 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
+                                .frame(width: 8, height: 8)
+                            
+                            Circle()
+                                .fill(tabIndex == 1 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
+                                .frame(width: 8, height: 8)
+                            
+                            Circle()
+                                .fill(tabIndex == 2 ? Color.systemGreen : Color.secondaryLabel.opacity(0.65))
+                                .frame(width: 8, height: 8)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            tabIndex += 1
+                        } label: {
+                            Image(systemName: "arrow.forward")
+                                .foregroundColor(Color.systemGreen)
+                                .font(.system(size: 16, weight: .regular))
+                        }
+                    }
+                    .padding(.bottom, 44)
+                    .padding(.horizontal, 16)
+                }
             }
+            .background(Color.systemGroupedBackground)
         }
-        .background(Color.systemGroupedBackground)
     }
 }
 
